fea3ee6c868d88fe4a65ab52b0e96e56726537f1
Daniel R. <danielrudcallesen96@gmail.com>
2025-09-26 13:55:33 +0200

    Added Models

---
+++ StudentManagement/Models/Enrollment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StudentManagement.Models;
+public class Class1
+{
+	public int Id { get; set; }
+	public int StudentId { get; set; }
+	public int CourseId { get; set; }
+	public int Grade { get; set; }
+}
---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentManagement.Models;
+
+public class Student
+{
+	public int Id { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string Email { get; set; }
+	public DateTime EnrollmentDate { get; set; }
+}
