46d1909b7cc17405a3d8872db92299594fb25581
Daniel R. <danielrudcallesen96@gmail.com>
2025-09-26 15:04:11 +0200

    V1_InitialSchema

--- StudentManagement/obj/Release/net8.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Release/net8.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da270afdaf5e6a7f4dfab752d92bced6d6f508ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd754cd5431e27ccb627d2152f9f295e40bba19")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
